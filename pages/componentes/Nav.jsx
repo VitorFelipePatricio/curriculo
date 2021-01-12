491d68d009d6576e9d5ad500e3daf65a119838fb
VitorFelipePatricio <vitor.felipeee88@gmail.com>
2021-01-12 04:39:31 -0300

    Nav iniciada

--- pages/componentes/Nav.jsx
+++ pages/componentes/Nav.jsx
@@ -1,34 +1,31 @@
 import React, { Component } from 'react'
 
-export default class Nav extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Vitor Felipe</h1>
-                <nav>
-                    <ul>
-                        <li>
-                            <p><a href="">EXPERIÊNCIAS</a></p>
-                        </li>
-                        <li>
-                            <p><a href="">FORMAÇÃO</a></p>
-                        </li>
-                        <li>
-                            <p><a href="">CLIENTES</a></p>
-                        </li>
-                        <li>
-                            <p><a href="">HABILIDADES</a></p>
-                        </li>
-                        <li>
-                            <p><a href="">ESPECIALIDADES</a></p>
-                        </li>
-                        <li>
-                            <p><a href="">CONTATO</a></p>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        )
-    }
-}
+export default () =>
+    <div>
+        <h1>Vitor Felipe</h1>
+        <nav>
+            <ul>
+                <li>
+                    <p><a href="">EXPERIÊNCIAS</a></p>
+                </li>
+                <li>
+                    <p><a href="">FORMAÇÃO</a></p>
+                </li>
+                <li>
+                    <p><a href="">CLIENTES</a></p>
+                </li>
+                <li>
+                    <p><a href="">HABILIDADES</a></p>
+                </li>
+                <li>
+                    <p><a href="">ESPECIALIDADES</a></p>
+                </li>
+                <li>
+                    <p><a href="">CONTATO</a></p>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+
 
