b35e1dec9bf81b7a26a6c4117d83a741aaaba148
VitorFelipePatricio <vitor.felipeee88@gmail.com>
2021-01-12 04:37:12 -0300

    Nav iniciada

--- pages/componentes/Nav.jsx
+++ pages/componentes/Nav.jsx
@@ -1,34 +1,32 @@
 import React, { Component } from 'react'
-import { Link , ItemLista, Titulo, Lista , NavBar } from './styles'
-
 
 export default class Nav extends Component {
     render() {
         return (
             <div>
-                <NavBar>
-                <Titulo>Vitor Felipe</Titulo>
-                    <Lista>
-                        <ItemLista>
-                            <p><Link href="#">EXPERIÊNCIAS</Link></p>
-                        </ItemLista>
-                        <ItemLista>
-                            <p><Link href="#">FORMAÇÃO</Link></p>
-                        </ItemLista>
-                        <ItemLista>
-                            <p><Link href="#">CLIENTES</Link></p>
-                        </ItemLista>
-                        <ItemLista>
-                            <p><Link href="#">HABILIDADES</Link></p>
-                        </ItemLista>
-                        <ItemLista>
-                            <p><Link href="#">ESPECIALIDADES</Link></p>
-                        </ItemLista>
-                        <ItemLista>
-                            <p><Link href="#">CONTATO</Link></p>
-                        </ItemLista>
-                    </Lista>
-                </NavBar>
+                <h1>Vitor Felipe</h1>
+                <nav>
+                    <ul>
+                        <li>
+                            <p><a href="">EXPERIÊNCIAS</a></p>
+                        </li>
+                        <li>
+                            <p><a href="">FORMAÇÃO</a></p>
+                        </li>
+                        <li>
+                            <p><a href="">CLIENTES</a></p>
+                        </li>
+                        <li>
+                            <p><a href="">HABILIDADES</a></p>
+                        </li>
+                        <li>
+                            <p><a href="">ESPECIALIDADES</a></p>
+                        </li>
+                        <li>
+                            <p><a href="">CONTATO</a></p>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         )
     }
