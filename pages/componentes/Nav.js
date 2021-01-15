c196eb766f7b58708f56bbd96f8659c5421224a6
VitorFelipePatricio <vitor.felipeee88@gmail.com>
2021-01-14 22:00:15 -0300

    2/3 nav finalizada

--- pages/componentes/Nav.js
+++ pages/componentes/Nav.js
@@ -1,28 +1,35 @@
 import React from 'react'
 
+function show() {
+    var menu = document.querySelector('.lista') 
+    menu.classList.toggle('show');
+}
+
 export default () =>
     <div className="container">
         <h1 className="titulo">Vitor Felipe</h1>
-        <nav className="nav">
-            <ul className="lista">
-                <li className="itemLista">
-                    <p><a className="link" href="">EXPERIÊNCIAS</a></p>
-                </li>
-                <li className="itemLista">
-                    <p><a className="link" href="">FORMAÇÃO</a></p>
-                </li>
-                <li className="itemLista">
-                    <p><a className="link" href="">CLIENTES</a></p>
-                </li>
-                <li className="itemLista">
-                    <p><a className="link" href="">HABILIDADES</a></p>
-                </li>
-                <li className="itemLista">
-                    <p><a className="link" href="">ESPECIALIDADES</a></p>
-                </li>
-                <li className="itemLista">
-                    <p><a className="link" href="">CONTATO</a></p>
-                </li>
-            </ul>
-        </nav>
+        <div className="menu">
+            <nav className="nav">
+                <ul className="lista">
+                    <li className="itemLista">
+                        <p><a onClick={show} className="link" href="#">EXPERIÊNCIAS</a></p>
+                    </li>
+                    <li className="itemLista">
+                        <p><a onClick={show} className="link" href="">FORMAÇÃO</a></p>
+                    </li>
+                    <li className="itemLista">
+                        <p><a onClick={show} className="link" href="">CLIENTES</a></p>
+                    </li>
+                    <li className="itemLista">
+                        <p><a onClick={show} className="link" href="">HABILIDADES</a></p>
+                    </li>
+                    <li className="itemLista">
+                        <p><a onClick={show} className="link" href="">ESPECIALIDADES</a></p>
+                    </li>
+                    <li className="itemLista">
+                        <p><a onClick={show} className="link" href="">CONTATO</a></p>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </div>
