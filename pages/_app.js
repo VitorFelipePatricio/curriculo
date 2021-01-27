37103bc55facf58e299bd0ceb26fed896e2b7ab2
VitorFelipePatricio <vitor.felipeee88@gmail.com>
2021-01-27 06:20:11 -0300

    home 2/3 finalizada

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import App from 'next/app'
 
 import '../styles/nav.css'
+import '../styles/home.css'
 
 export default App
---
+++ pages/components/Home.js
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGlobeEurope } from '@fortawesome/free-solid-svg-icons'
+
+export default () =>
+    <div>
+        <div className="background-image">
+            <div className="container-home">
+                <div>
+                    <div className="background-vitor"></div>
+                </div>
+                <div>
+                    <div className="container-home-dados">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="container-home-redes-sociais">
+        </div>
+    </div >
